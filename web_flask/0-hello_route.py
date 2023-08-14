893148d8b17755573733d0fe6f5aaf4d0dd9d549
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-14 13:43:21 -0500

    Update file 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.router('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
