b81650f0a9657babc533326723d0bc1fa1069006
RM92023 <robinjara20@gmail.com>
2023-08-17 17:42:14 -0500

    Adding new route python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Inicialization of the Python Flask application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+'''Route root URL'''
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Display hello World'''
+    return "Hello HBNB!"
+
+
+'''Route for /hbnb'''
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Display hello HBNB!'''
+    return "HBNB"
+
+
+'''Route for /hbnb'''
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''Display "C" followed by the value of <text>'''
+    '''Replace _ with "" in variables'''
+    formatText = text.replace('_', ' ')
+    return "C {}".format(formatText)
+
+
+'''Route for c/hbnb'''
+
+
+@app.route('/python/', default={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''Display "python" followed by the value of <text>'''
+    '''Replace _ with " " in variables'''
+    formatText = text.replace('_', ' ')
+    return "C {}".format(formatText)
+
+
+if __name__ == '__main__':
+    # start Flask server
+    app.run(debug=True, port=5000, host="0.0.0.0")
