d4cebf8cbee73f4b02da21b09e1abd15c94e936a
RM92023 <robinjara20@gmail.com>
2023-08-18 10:03:26 -0500

    update code in place and html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,7 +23,7 @@ def teardown(self):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     '''Display an HTML page with a list of all states'''
-    states = render_template(storage.all(State).values(), key=lambda s: s.name)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
